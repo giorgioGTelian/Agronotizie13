9dd2469e733bcd9f51d1fb7dcc6c5ce7526c2274
giorgio <giorgiobiologist@gmail.com>
2022-11-01 14:34:23 +0100

    eh

--- app/src/main/java/com/example/agronotizie13/MainActivity.java
+++ app/src/main/java/com/example/agronotizie13/MainActivity.java
@@ -1,33 +1,34 @@
 package com.example.agronotizie13;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
+
+import android.os.Build;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.os.Bundle;
 import android.webkit.WebViewClient;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
-        webView.getSettings().setBlockNetworkLoads(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://agronotizie.imagelinenetwork.com/");
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-
-
+        webview =(WebView)findViewById(R.id.webView);
+        webview.getSettings().setBlockNetworkLoads(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("https://www.google.com");
+        if (Build.VERSION.SDK_INT >= 19) {
+            webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
     }
-
-
 }
